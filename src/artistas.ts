e0446ed56d893b5c276cd7821251ba50fb168582
Noelia Ibañez <alu0101225555@ull.edu.es>
2022-03-31 14:42:40 +0000

    Clases artista y generos musicales añadidas

--- src/artistas.ts
+++ src/artistas.ts
@@ -1,15 +1,74 @@
+import { Album } from "./album";
+import { Cancion } from "./cancion";
+import { GenerosMusicales } from "./generosMusical";
+import { Grupos } from "./grupos";
 import { Libreria } from "./Libreria";
+
 //Noe
-export class Artista extends Libreria<T> {
 
-    //Nombre del artista.
-    //Grupos a los que pertenece.
-    //Género(s) músicales relacionados.
-    //Álbumes en los que ha participado.
-    //Canciones publicadas.
-    //Cantidad de oyentes mensuales.
+/**
+ * Clase Artista para identificar artistas que forman parte de grupos y además tienen carreras en solitario o que están en varios grupos.
+ */
+export class Artista {
 
-    constructor() {
-        super();
+    /**
+     * Constructor con los siguientes parámetros
+     * @param nombreArtista Nombre del artista
+     * @param gruposArtista Grupos a los que pertenece
+     * @param generosArtista Género(s) músicales relacionados
+     * @param albumesArtista Álbumes en los que ha participado
+     * @param cancionesArtista Canciones publicadas
+     * @param oyentesArtista Cantidad de oyentes mensuales
+     */
+    constructor(private nombreArtista: string, private gruposArtista: Grupos[], private generosArtista: GenerosMusicales[], private albumesArtista: Album[], private cancionesArtista: Cancion[], private oyentesArtista: number) {}
+
+    /**
+     * Método que devuelve el nombre de un artista
+     * @returns nombre artista
+     */
+    getNombreArtista(): string {
+        return this.nombreArtista;
     }
+
+    /**
+     * Método que devuelve los grupos a los que pertenece un artista
+     * @returns grupos a los que pertenece un artista
+     */
+    getGruposArtista(): Grupos[] {
+        return this.gruposArtista;
+    }
+
+    /**
+     * Método que devuelve los géneros relacionados con un artista
+     * @returns géneros relacionados 
+     */
+    getGenerosArtista(): GenerosMusicales[] {
+        return this.generosArtista;
+    }
+
+    /**
+     * Método que devuelve los albumes en los que ha participado un artista
+     * @returns albumes en los que ha participado
+     */
+    getAlbumesArtista(): Album[] {
+        return this.albumesArtista;
+    }
+
+    /**
+     * Método que devuelve las canciones de un artista
+     * @returns canciones de un artista
+     */
+    getCancionesArtista(): Cancion[] {
+        return this.cancionesArtista;
+    }
+
+    /**
+     * Método que devuelve los oyentes mensuales de un artista
+     * @returns oyentes mensuales 
+     */
+    getOyentesArtista(): number {
+        return this.oyentesArtista;
+    }
+
+    //Falta cálculo oyentes
 }
--- src/generosMusical.ts
+++ src/generosMusical.ts
@@ -1,14 +1,50 @@
+import { Album } from "./album";
+import { Cancion } from "./cancion";
 import { Libreria } from "./Libreria";
 //Noe 
 
 
-export class GenerosMusicales extends Libreria<T> {
-    constructor() {
-        super();
+export class GenerosMusicales {
+
+    /**
+     * Constructor
+     * @param genero Nombre del género
+     * @param grupos_artistas Grupos y/o artistas producen música de ese género
+     * @param generoAlbumes Álbumes que hay dentro de la biblioteca relacionados con este género
+     * @param generoCanciones Canciones que hay dentro de la biblioteca de ese género
+     */
+    constructor(private genero: string, private grupos_artistas: string[], private generoAlbumes: Album[], private generoCanciones: Cancion[] ) {}
+
+    /**
+     * Método que devuelve el nombre del género
+     * @returns genero
+     */
+    getGenero(): string {
+        return this.genero;
     }
-    //Nombre del género.
-    //Grupos y/o artistas producen música de ese género.
-    //Álbumes que hay dentro de la biblioteca relacionados con este género.
-    //Canciones que hay dentro de la biblioteca de ese género.
+
+    /**
+     * Método que devuelve los grupos y/o artistas que producen música de ese género
+     * @returns grupos y/o artistas
+     */
+    getGruposArtistas(): string[] {
+        return this.grupos_artistas;
+    }
+
+    /**
+     * Método que devuelve los albumes que pertenecen a ese género
+     * @returns albumes pertenecientes a ese género
+     */
+    getGeneroAlbumes(): Album[] {
+        return this.generoAlbumes;
+    }
+
+    /**
+     * Método que devuelve las canciones que pertenecen a ese género
+     * @returns canciones pertenecientes a ese género
+     */
+    getGeneroCanciones(): Cancion[] {
+        return this.generoCanciones;
+    }
 
 }
