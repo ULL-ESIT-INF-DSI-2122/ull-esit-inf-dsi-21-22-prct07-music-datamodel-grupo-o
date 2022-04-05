322c792de81885696674b5683e4e6356b232d235
Stephanie Arismendi <alu0101351728@ull.edu.es>
2022-04-05 22:05:30 +0000

    espacios

--- tests/playlist.spec.ts
+++ tests/playlist.spec.ts
@@ -1,43 +1,42 @@
 import 'mocha';
-import { expect } from 'chai';
-import { Playlist } from '../src/playlist';
-import { Cancion } from '../src/cancion';
+import {expect} from 'chai';
+import {Playlist} from '../src/playlist';
+import {Cancion} from '../src/cancion';
 
-describe('Test para la clase Playlist' , () => {
-    
-    // constructor(private nombre:string, private autor:string, private duracion:number, 
-    //     private generos: string[], private single:boolean, private numeroReproducciones:number) {
-    //   }
-    
-    const stay:Cancion = new Cancion('Stay', ['Justin Bieber'], 141, ['pop-rock', 'hip-hop'], true, 200);
-    const butter:Cancion = new Cancion('Butter', ['BTS'], 164, ['dance-pop', 'k-pop'], true, 100);
+describe('Test para la clase Playlist', () => {
+  // constructor(private nombre:string, private autor:string, private duracion:number,
+  //     private generos: string[], private single:boolean, private numeroReproducciones:number) {
+  //   }
 
-    // constructor(private nombre:string, private canciones:Cancion[]) {
-    //     // Nombre de la playlist.
-    //     // Canciones incluidas dentro de la playlist.
-    //     // Duración en horas y minutos.         //private duracion:number, 
-    //     // Género(s) musicales que se incluyen dentro de la playlist.
-    // }
+  const stay:Cancion = new Cancion('Stay', ['Justin Bieber'], 141, ['pop-rock', 'hip-hop'], true, 200);
+  const butter:Cancion = new Cancion('Butter', ['BTS'], 164, ['dance-pop', 'k-pop'], true, 100);
 
-    const collection1 = [stay, butter];
-    // collection1.addCanciones(stay);
-    // collection1.addCanciones(butter);
+  // constructor(private nombre:string, private canciones:Cancion[]) {
+  //     // Nombre de la playlist.
+  //     // Canciones incluidas dentro de la playlist.
+  //     // Duración en horas y minutos.         //private duracion:number,
+  //     // Género(s) musicales que se incluyen dentro de la playlist.
+  // }
 
-    const playlist1 = new Playlist("Mejores canciones 2022", collection1);
+  const collection1 = [stay, butter];
+  // collection1.addCanciones(stay);
+  // collection1.addCanciones(butter);
 
-    it("Test para la clase Playlist", () => {
-        expect(playlist1).not.to.be.null;
-    });
+  const playlist1 = new Playlist("Mejores canciones 2022", collection1);
 
-    it("Test obtener nombre de la lista", () => {
-        expect(playlist1.getNombre()).to.eq("Mejores canciones 2022");
-    });
+  it("Test para la clase Playlist", () => {
+    expect(playlist1).not.to.be.null;
+  });
 
-    it("Test obtener la duracion de la lista", () => {
-        expect(playlist1.getDuracion()).to.eq(305);
-    });
+  it("Test obtener nombre de la lista", () => {
+    expect(playlist1.getNombre()).to.eq("Mejores canciones 2022");
+  });
 
-    it("Test obtener generos de la lista", () => {
-        // expect(playlist1.getGenerosMusicales()).to.eqls(["pop-rock", "hip-hop"]);
-    });
+  it("Test obtener la duracion de la lista", () => {
+    expect(playlist1.getDuracion()).to.eq(305);
+  });
+
+  it("Test obtener generos de la lista", () => {
+    // expect(playlist1.getGenerosMusicales()).to.eqls(["pop-rock", "hip-hop"]);
+  });
 });
